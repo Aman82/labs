f94424386423a38df59baff212996ada49173f01
Aman82 <jotdhillon@gmail.com>
2014-09-11 13:56:30 -0700

    working with auth and sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,29 @@
 class SessionsController < ApplicationController
 
 	def new
+		flash.clear
 	end
 
 	def create
-		# STEP ONE - Retrieve a User record by the email parameter
+		flash.clear
 		
-		# STEP TWO - If the user exists, use the User.authenticate 
-		#            method to ensure the password is correct
-		
-		# STEP THREE - If authenticated successfully, redirect to root_path
+		begin
+			user = User.find_by({email: params[:session][:email]})
+		rescue
+			flash[:error] = 'Email not found.'			
+		end
+
+		if user && user.authenticate(params[:session][:password])
+			log_in(user)
+			redirect_to root_path
+		else
+			flash[:error] ||= 'Try again.'
+			render 'new'
+		end
 	end
 
 	def destroy
+		log_out
+		redirect_to sessions_path
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 LoggyInny::Application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root :to => "users#new"
 
   ########################
   # SESSIONS RESOURCE
   ########################
-
+  
+  get 'login/new' => 'sessions#new'
+  post 'login/' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   ########################
   # USERS RESOURCE
